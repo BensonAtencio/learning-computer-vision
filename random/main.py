81693d527fba349b68ecc0e5a5d3d55c6b5e6b1f
BensonAtencio <benson.atencio@g.batstate-u.edu.ph>
2022-09-14 12:52:44 +0800

    resizing and rotating images

--- random/main.py
+++ random/main.py
@@ -1,6 +1,9 @@
 import cv2
 
-img = cv2.imread('assets/Facebook-logo.png', 0)
+img = cv2.imread('C:\\Users\\MARASIGAN\\repository\\learning-computer-vision\\random\\assets\\Facebook-logo.png', 0)
+img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+# img = cv2.rotate(img, cv2.ROTATE_180)
+# cv2.imwrite('new-fb-logo.png', img)
 
 cv2.imshow('Image', img)
 cv2.waitKey(0)
